a16d2b70fcbf3112ec236b244355e5126c27cad3
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-08-24 05:21:18 +1000

    application of optional chaining

--- src/components/PropsDrilling/UserContainer.jsx
+++ src/components/PropsDrilling/UserContainer.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 const UserContainer = ({ user, logout }) => {
   return (
     <div className="user-container">
-      <p>Hello there, {user?.name?.toUpperCase()}</p>
-      <button className="btn" onClick={logout}>
-        logout
-      </button>
+      {user ? (
+        <>
+          <p>Hello there, {user?.name?.toUpperCase()}</p>
+          <button className="btn" onClick={logout}>
+            logout
+          </button>
+        </>
+      ) : (
+        <p>Please Login</p>
+      )}
     </div>
   );
 };
